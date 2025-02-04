53aa7ade5d22f0fa238f4ab73e5ac0636677b7b9
David Robert Lewis <ubuntupunk@gmail.com>
2025-02-04 17:55:24 +0200

    added setup, readme

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='gpgqr',
+    name='gpg-qr',
     version='0.1.0',
     packages=find_packages('src'),
     package_dir={'': 'src'},
@@ -15,9 +15,9 @@ setup(
     ],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/your-username/gpgqr",
-    author="Your Name",
-    author_email="your.email@example.com",
+    url="https://github.com/ubuntupunk/gpg-qr",
+    author="David Robert Lewis",
+    author_email="ubuntupunk@gmail.com",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
